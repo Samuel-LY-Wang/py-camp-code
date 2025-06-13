146cd621e59882d3777dd451cb16c3f7d84284da
Samuel-LY-Wang <70108412+Samuel-LY-Wang@users.noreply.github.com>
2025-06-13 14:56:40 -0400

    removed my personal info

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-FROM="wamsang69@gmail.com"
+FROM=""
 SERVER = "smtp.gmail.com"
 PORT = 587
-PASS = "vgzhljdpilelkkui"
-WEATHER_API_KEY="73f5a2ef8432c3f8afe22dc854be41fb"
+PASS = ""
+WEATHER_API_KEY=""
